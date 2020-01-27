d3c47c156d502229ceda6001dcc09b631a90e3ae
Julie Nguyen <julie-nguyen@live.com>
2020-01-27 20:39:43 +0000

    created tests using mock data for application

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -48,9 +48,9 @@ export default function useApplicationData() {
 
   useEffect(() => {
     Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ])
     .then((all) => {
       dispatch({ type: GET_DATA, value: { days: all[0].data, appointments : all[1].data, interviewers: all[2].data}})
