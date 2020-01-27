a0dd1f0f2690fb0e9a1f4932d4c34b9269cde210
Julie Nguyen <julie-nguyen@live.com>
2020-01-27 21:43:54 +0000

    used prettyDOM

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,16 +1,24 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, prettyDOM } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
-  return waitForElement(() => getByText("Monday"))
-        .then(() =>{
-          fireEvent.click(getByText("Tuesday"));
-          expect(getByText("Leopold Silvers")).toBeInTheDocument();
-        });
-});
+describe("Application", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+    
+    await waitForElement(() => getByText("Monday"))
+  
+    fireEvent.click(getByText("Tuesday"));
+  
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+  
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container } = render(<Application />);
+    console.log(prettyDOM(container));
+  })
+})
